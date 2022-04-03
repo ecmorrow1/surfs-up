284f5cc594352b97191a2f92079bef32e0910ec2
Morrow <edward.c.morrow@gmail.com>
2022-04-02 18:38:14 -0700

    completed deliverables 1 and 2

--- app.py
+++ app.py
@@ -1,15 +1,85 @@
 # Import dependencies
-from flask import Flask
+import datetime as dt
+from distutils.log import debug
+import numpy as np
+import pandas as pd
 
-# Note: Variables with underscores before an after them are called "magic methods" in Python.
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
-# Create first Flask app instance
+from flask import Flask, jsonify
+
+# Create database engine for the Flask application
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect the database into classes
+Base = automap_base()
+Base.prepare(engine,reflect=True)
+
+# Create a variable for each of the classes
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create a session link from Python to the database
+session = Session(engine)
+
+# Create a Flask application named "app"
 app = Flask(__name__)
 
-# __name__ denotes the name of the current function.  Use __name__ to determine if the code is being run from the command line or if it has been imported into another piece of code.
+@app.route("/")
+def welcome():
+    return(
+    '''<plaintext>
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
 
-# Create a Flask route.  First, define a starting point, which is known as the "root".  To do this, use @app.route('/')
-# the forward slash inside of app.route() denoates that the data will root to our routes.  The forward slash is commonly known as the highest level of hierarchy in any computer system.
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
